dc7b2a03124d1fee0b99bc44d248c59043c3d1b7
Tiberiu MAXIM <tiberiu.maxim1@my.fmi.unibuc.ro>
2021-03-27 22:32:06 +0200

    Refactor Reader Interface

--- src/main/java/javabank/reader/Reader.java
+++ src/main/java/javabank/reader/Reader.java
@@ -3,6 +3,7 @@ package javabank.reader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
 
 public interface Reader<E> {
     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -11,6 +12,7 @@ public interface Reader<E> {
      * Abstract method that reads an entity from the System.in
      * @return E, representing the read entity
      * @throws IOException, representing an exception
+     * @throws ParseException, representing an exception
      */
-    E read() throws IOException;
+    E read() throws IOException, ParseException;
 }
