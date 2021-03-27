7c5d92688319dd14392c504720c100757ad8c794
Tiberiu MAXIM <tiberiu.maxim1@my.fmi.unibuc.ro>
2021-03-27 19:23:51 +0200

    Create BankAddressReader.java

---
+++ src/main/java/javabank/reader/BankAddressReader.java
@@ -0,0 +1,42 @@
+package javabank.reader;
+
+import javabank.domain.BankAddress;
+
+import java.io.IOException;
+
+public class BankAddressReader implements Reader<BankAddress>{
+    /**
+     * Method that reads a BankAddress from the System.in
+     * @return BankAddress, representing the read BankAddress
+     * @throws IOException, representing an exception
+     */
+    @Override
+    public BankAddress read() throws IOException {
+        System.out.println("Please introduce the bank address! ");
+        String streetName;
+        System.out.print("Street name: ");
+        streetName = bufferedReader.readLine();
+
+        int number;
+        System.out.print("Building number: ");
+        number = Integer.parseInt(bufferedReader.readLine());
+
+        String cityName;
+        System.out.print("City: ");
+        cityName = bufferedReader.readLine();
+
+        String countyName;
+        System.out.print("County: ");
+        countyName = bufferedReader.readLine();
+
+        int postalCode;
+        System.out.print("Postal Code: ");
+        postalCode = Integer.parseInt(bufferedReader.readLine());
+
+        String countryName;
+        System.out.print("Country: ");
+        countryName = bufferedReader.readLine();
+
+        return new BankAddress(streetName, number, cityName, countyName, postalCode, countryName);
+    }
+}
