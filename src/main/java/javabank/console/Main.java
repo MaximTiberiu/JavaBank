99d279b0f1ea3284b6cf48dfe9ce49c4c51b908b
Tiberiu MAXIM <tiberiu.maxim1@my.fmi.unibuc.ro>
2021-03-28 17:19:47 +0300

    Update Main.java

--- src/main/java/javabank/console/Main.java
+++ src/main/java/javabank/console/Main.java
@@ -1,7 +1,12 @@
 package javabank.console;
 
+import javabank.UI.UI;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        UI consoleApplication = new UI();
+        consoleApplication.startApp();
     }
 }
